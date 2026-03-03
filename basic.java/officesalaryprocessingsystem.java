3657e7e4eab09cbb5ca7c687021f48bab07f2466
Vimal-5 <vimalvijay561@gmail.com>
2026-03-03 20:45:36 +0530

    Add files via upload

---
+++ basic.java/officesalaryprocessingsystem.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class officesalaryprocessingsystem {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int a=obj.nextInt();
+        int b=obj.nextInt();
+        int c=obj.nextInt();
+        int d=obj.nextInt();
+        int e=obj.nextInt();
+        System.out.println(a+b+c-d-e);
+    }
+}
