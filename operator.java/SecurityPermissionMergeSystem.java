ae8d0e825402131985144b3a112aef817642077c
Vimal-5 <vimalvijay561@gmail.com>
2026-03-06 22:07:51 +0530

    Add files via upload

---
+++ operator.java/SecurityPermissionMergeSystem.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class SecurityPermissionMergeSystem {
+    public static void main(String[] args) {
+        Scanner object=new Scanner(System.in);
+        int permission1=object.nextInt();
+        int permission2=object.nextInt();
+        System.out.println(permission1+permission2);
+    }
+}
