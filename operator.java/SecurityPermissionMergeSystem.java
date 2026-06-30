48683a7461e75ff722064488906eef436e49942f
Vimal-5 <vimalvijay561@gmail.com>
2026-06-30 20:28:55 +0530

    Delete operator.java/SecurityPermissionMergeSystem.java

--- operator.java/SecurityPermissionMergeSystem.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class SecurityPermissionMergeSystem {
-    public static void main(String[] args) {
-        Scanner object=new Scanner(System.in);
-        int permission1=object.nextInt();
-        int permission2=object.nextInt();
-        System.out.println(permission1+permission2);
-    }
-}
