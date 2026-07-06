34d012d8682ce4f0c49db2a42d6ba04f4415d67d
Vimal-5 <vimalvijay561@gmail.com>
2026-07-06 07:39:44 +0530

    Add files via upload

---
+++ Inlustro Training java Program/List.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.remove(1);
+        list.remove(Integer.valueOf(40));
+        System.out.println(list);
+    }
+}
