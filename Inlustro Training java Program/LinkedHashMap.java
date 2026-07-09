6c5ff872e13f96ddb35dbb18c207b985a26c3bbd
Vimal-5 <vimalvijay561@gmail.com>
2026-07-09 07:42:44 +0530

    Add files via upload

---
+++ Inlustro Training java Program/LinkedHashMap.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		LinkedHashMap<Integer , String> hm = new LinkedHashMap<>();
+		
+		hm.put(2,"vimal");
+		hm.put(1,"vijay");
+		hm.put(3,"ajay");
+		
+		System.out.println(hm);
+		System.out.println(hm.get(2));
+	}
+}
